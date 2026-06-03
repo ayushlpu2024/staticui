13232208363364f73e62f345504106b075fae80c
ayushlpu2024 <ayuush8757@gmail.com>
2026-06-03 16:42:44 +0530

    bypass build error

--- src/lib/razorpay.ts
+++ src/lib/razorpay.ts
@@ -1,6 +1,6 @@
 import Razorpay from 'razorpay';
 
 export const razorpay = new Razorpay({
-  key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID || '',
-  key_secret: process.env.RAZORPAY_KEY_SECRET || '',
+  key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID || 'dummy_key_id_to_bypass_build_error',
+  key_secret: process.env.RAZORPAY_KEY_SECRET || 'dummy_secret_to_bypass_build_error',
 });
